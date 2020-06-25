14f53ecc0cfd7c5cab9e37a165ce68b4e0dc04f9
Reinier de Leon <z5257456@unsw.edu.au>
2020-06-25 18:56:33 +1000

    Made comments of what to do

--- src/unsw/venues/VenueHireSystem.java
+++ src/unsw/venues/VenueHireSystem.java
@@ -51,12 +51,41 @@ public class VenueHireSystem {
             System.out.println(result.toString(2));
             break;
 
-        // TODO Implement other commands
+        case "change":
+            String id2 = json.getString("id");
+            LocalDate start2 = LocalDate.parse(json.getString("start"));
+            LocalDate end2 = LocalDate.parse(json.getString("end"));
+            int small2 = json.getInt("small");
+            int medium2 = json.getInt("medium");
+            int large2 = json.getInt("large");
+        
+            // 1. loop through each venue then check room's
+            // reservedList and check each reservation with
+            // same id then delete that from reservedList
+            // (have a counter for s, m, l depending on prev
+            // reservation and when count is met, stop searching)
+            // 2. then use request command to add new reservation
+            // (if possible)
+            // 3. do System.out.println(blahblah)
+            break;
+        case "cancel":
+            String id3 = json.getString("id");
+            // 1. same as step 1 in "change" command
+            // 2. then step 3 of "change" command
+            break;
+        case "list":
+            String venue2 = json.getString("venue");
+            // loop through venue's roomList and output
+            // reservedList for each and an empty ArrayList
+            // if nothing in it
         }
     }
 
     private void addRoom(String venue, String room, String size) {
         // TODO Process the room command
+        // 1. create a room in room class with size given
+        // 2. create a venue in venue class
+        // 2. go into venue object then append room to roomList
     }
 
     public JSONObject request(String id, LocalDate start, LocalDate end,
@@ -64,6 +93,11 @@ public class VenueHireSystem {
         JSONObject result = new JSONObject();
 
         // TODO Process the request commmand
+        // go into venue then into room reservedList
+        // then compare dates and
+        // sizes of every room and see if there is space
+        // if there is result.put success with venue etc
+        // but status reject if found clashing times
 
         // FIXME Shouldn't always produce the same answer
         result.put("status", "success");
